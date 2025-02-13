00926dc4ce64054f0a14aa5337589269486e60c4
wardrockay <tolliam.e@gmail.com>
2025-02-13 09:44:09 +0100

    add picture

--- front/src/app/product/[slug]/page.tsx
+++ front/src/app/product/[slug]/page.tsx
@@ -16,6 +16,13 @@ const ProductPage: React.FC<{ product: Product }> = ({ product }) => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <Card className="max-w-md w-full p-4 bg-white shadow-lg rounded-2xl">
+        <Image
+          src="/images/nft1.webp"
+          alt="test"
+          width={400}
+          height={300}
+          className="rounded-lg object-cover"
+        />
         <CardContent className="p-4">
           <h1 className="text-xl font-semibold">test</h1>
           <p className="text-gray-600 my-2">ceci est une description</p>
