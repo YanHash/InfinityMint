67863e62b818b6e0cbd430fae33dbd9cf19a12d1
wardrockay <tolliam.e@gmail.com>
2025-03-12 16:56:43 +0100

    add route to minting and Marketplace

--- front/src/components/ui/menubar.tsx
+++ front/src/components/ui/menubar.tsx
@@ -2,48 +2,48 @@
 
 import * as React from "react"
 import * as MenubarPrimitive from "@radix-ui/react-menubar"
-import {Check, ChevronRight, Circle, SearchIcon} from "lucide-react"
+import { Check, ChevronRight, Circle, SearchIcon } from "lucide-react"
 
-import {cn} from "@/lib/utils"
+import { cn } from "@/lib/utils"
 
-import {Avatar, AvatarFallback, AvatarImage,} from "@/components/ui/avatar"
-import {useRouter} from 'next/navigation'
-import {Input} from "@/components/ui/input";
+import { Avatar, AvatarFallback, AvatarImage, } from "@/components/ui/avatar"
+import { useRouter } from 'next/navigation'
+import { Input } from "@/components/ui/input";
 
 function MenubarMenu({
-                         ...props
-                     }: React.ComponentProps<typeof MenubarPrimitive.Menu>) {
+    ...props
+}: React.ComponentProps<typeof MenubarPrimitive.Menu>) {
     return <MenubarPrimitive.Menu {...props} />
 }
 
 function MenubarGroup({
-                          ...props
-                      }: React.ComponentProps<typeof MenubarPrimitive.Group>) {
+    ...props
+}: React.ComponentProps<typeof MenubarPrimitive.Group>) {
     return <MenubarPrimitive.Group {...props} />
 }
 
 function MenubarPortal({
-                           ...props
-                       }: React.ComponentProps<typeof MenubarPrimitive.Portal>) {
+    ...props
+}: React.ComponentProps<typeof MenubarPrimitive.Portal>) {
     return <MenubarPrimitive.Portal {...props} />
 }
 
 function MenubarRadioGroup({
-                               ...props
-                           }: React.ComponentProps<typeof MenubarPrimitive.RadioGroup>) {
+    ...props
+}: React.ComponentProps<typeof MenubarPrimitive.RadioGroup>) {
     return <MenubarPrimitive.RadioGroup {...props} />
 }
 
 function MenubarSub({
-                        ...props
-                    }: React.ComponentProps<typeof MenubarPrimitive.Sub>) {
+    ...props
+}: React.ComponentProps<typeof MenubarPrimitive.Sub>) {
     return <MenubarPrimitive.Sub data-slot="menubar-sub" {...props} />
 }
 
 const Menubar = React.forwardRef<
     React.ElementRef<typeof MenubarPrimitive.Root>,
     React.ComponentPropsWithoutRef<typeof MenubarPrimitive.Root>
->(({className, ...props}, ref) => (
+>(({ className, ...props }, ref) => (
     <MenubarPrimitive.Root
         ref={ref}
         className={cn(
@@ -58,7 +58,7 @@ Menubar.displayName = MenubarPrimitive.Root.displayName
 const MenubarMain = React.forwardRef<
     React.ElementRef<typeof MenubarPrimitive.Root>,
     React.ComponentPropsWithoutRef<typeof MenubarPrimitive.Root>
->(({className, ...props}, ref) => (
+>(({ className, ...props }, ref) => (
     <MenubarPrimitive.Root
         ref={ref}
         className={cn(
@@ -74,7 +74,7 @@ MenubarMain.displayName = "MenubarMain"
 const MenubarTrigger = React.forwardRef<
     React.ElementRef<typeof MenubarPrimitive.Trigger>,
     React.ComponentPropsWithoutRef<typeof MenubarPrimitive.Trigger>
->(({className, ...props}, ref) => (
+>(({ className, ...props }, ref) => (
     <MenubarPrimitive.Trigger
         ref={ref}
         className={cn(
@@ -89,9 +89,9 @@ MenubarTrigger.displayName = MenubarPrimitive.Trigger.displayName
 const MenubarSubTrigger = React.forwardRef<
     React.ElementRef<typeof MenubarPrimitive.SubTrigger>,
     React.ComponentPropsWithoutRef<typeof MenubarPrimitive.SubTrigger> & {
-    inset?: boolean
-}
->(({className, inset, children, ...props}, ref) => (
+        inset?: boolean
+    }
+>(({ className, inset, children, ...props }, ref) => (
     <MenubarPrimitive.SubTrigger
         ref={ref}
         className={cn(
@@ -102,7 +102,7 @@ const MenubarSubTrigger = React.forwardRef<
         {...props}
     >
         {children}
-        <ChevronRight className="ml-auto h-4 w-4"/>
+        <ChevronRight className="ml-auto h-4 w-4" />
     </MenubarPrimitive.SubTrigger>
 ))
 MenubarSubTrigger.displayName = MenubarPrimitive.SubTrigger.displayName
@@ -110,7 +110,7 @@ MenubarSubTrigger.displayName = MenubarPrimitive.SubTrigger.displayName
 const MenubarSubContent = React.forwardRef<
     React.ElementRef<typeof MenubarPrimitive.SubContent>,
     React.ComponentPropsWithoutRef<typeof MenubarPrimitive.SubContent>
->(({className, ...props}, ref) => (
+>(({ className, ...props }, ref) => (
     <MenubarPrimitive.SubContent
         ref={ref}
         className={cn(
@@ -127,7 +127,7 @@ const MenubarContent = React.forwardRef<
     React.ComponentPropsWithoutRef<typeof MenubarPrimitive.Content>
 >(
     (
-        {className, align = "start", alignOffset = -4, sideOffset = 8, ...props},
+        { className, align = "start", alignOffset = -4, sideOffset = 8, ...props },
         ref
     ) => (
         <MenubarPrimitive.Portal>
@@ -150,9 +150,9 @@ MenubarContent.displayName = MenubarPrimitive.Content.displayName
 const MenubarItem = React.forwardRef<
     React.ElementRef<typeof MenubarPrimitive.Item>,
     React.ComponentPropsWithoutRef<typeof MenubarPrimitive.Item> & {
-    inset?: boolean
-}
->(({className, inset, ...props}, ref) => (
+        inset?: boolean
+    }
+>(({ className, inset, ...props }, ref) => (
     <MenubarPrimitive.Item
         ref={ref}
         className={cn(
@@ -168,7 +168,7 @@ MenubarItem.displayName = MenubarPrimitive.Item.displayName
 const MenubarCheckboxItem = React.forwardRef<
     React.ElementRef<typeof MenubarPrimitive.CheckboxItem>,
     React.ComponentPropsWithoutRef<typeof MenubarPrimitive.CheckboxItem>
->(({className, children, checked, ...props}, ref) => (
+>(({ className, children, checked, ...props }, ref) => (
     <MenubarPrimitive.CheckboxItem
         ref={ref}
         className={cn(
@@ -178,11 +178,11 @@ const MenubarCheckboxItem = React.forwardRef<
         checked={checked}
         {...props}
     >
-    <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
-      <MenubarPrimitive.ItemIndicator>
-        <Check className="h-4 w-4"/>
-      </MenubarPrimitive.ItemIndicator>
-    </span>
+        <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
+            <MenubarPrimitive.ItemIndicator>
+                <Check className="h-4 w-4" />
+            </MenubarPrimitive.ItemIndicator>
+        </span>
         {children}
     </MenubarPrimitive.CheckboxItem>
 ))
@@ -191,7 +191,7 @@ MenubarCheckboxItem.displayName = MenubarPrimitive.CheckboxItem.displayName
 const MenubarRadioItem = React.forwardRef<
     React.ElementRef<typeof MenubarPrimitive.RadioItem>,
     React.ComponentPropsWithoutRef<typeof MenubarPrimitive.RadioItem>
->(({className, children, ...props}, ref) => (
+>(({ className, children, ...props }, ref) => (
     <MenubarPrimitive.RadioItem
         ref={ref}
         className={cn(
@@ -200,11 +200,11 @@ const MenubarRadioItem = React.forwardRef<
         )}
         {...props}
     >
-    <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
-      <MenubarPrimitive.ItemIndicator>
-        <Circle className="h-4 w-4 fill-current"/>
-      </MenubarPrimitive.ItemIndicator>
-    </span>
+        <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
+            <MenubarPrimitive.ItemIndicator>
+                <Circle className="h-4 w-4 fill-current" />
+            </MenubarPrimitive.ItemIndicator>
+        </span>
         {children}
     </MenubarPrimitive.RadioItem>
 ))
@@ -213,9 +213,9 @@ MenubarRadioItem.displayName = MenubarPrimitive.RadioItem.displayName
 const MenubarLabel = React.forwardRef<
     React.ElementRef<typeof MenubarPrimitive.Label>,
     React.ComponentPropsWithoutRef<typeof MenubarPrimitive.Label> & {
-    inset?: boolean
-}
->(({className, inset, ...props}, ref) => (
+        inset?: boolean
+    }
+>(({ className, inset, ...props }, ref) => (
     <MenubarPrimitive.Label
         ref={ref}
         className={cn(
@@ -231,7 +231,7 @@ MenubarLabel.displayName = MenubarPrimitive.Label.displayName
 const MenubarSeparator = React.forwardRef<
     React.ElementRef<typeof MenubarPrimitive.Separator>,
     React.ComponentPropsWithoutRef<typeof MenubarPrimitive.Separator>
->(({className, ...props}, ref) => (
+>(({ className, ...props }, ref) => (
     <MenubarPrimitive.Separator
         ref={ref}
         className={cn("-mx-1 my-1 h-px bg-muted", className)}
@@ -241,9 +241,9 @@ const MenubarSeparator = React.forwardRef<
 MenubarSeparator.displayName = MenubarPrimitive.Separator.displayName
 
 const MenubarShortcut = ({
-                             className,
-                             ...props
-                         }: React.HTMLAttributes<HTMLSpanElement>) => {
+    className,
+    ...props
+}: React.HTMLAttributes<HTMLSpanElement>) => {
     return (
         <span
             className={cn(
@@ -270,11 +270,14 @@ const MainMenubar = () => {
             <MenubarMenu>
                 <MenubarTrigger>NFT</MenubarTrigger>
                 <MenubarContent>
-                    <MenubarItem>
+                    <MenubarItem onClick={() => router.push('/minting')}>
                         Ajouter une NFT
                     </MenubarItem>
+                    <MenubarItem onClick={() => router.push('/Marketplace')}>
+                        Lister un NFT
+                    </MenubarItem>
                     <MenubarItem disabled>Expertiser une NFT</MenubarItem>
-                    <MenubarSeparator/>
+                    <MenubarSeparator />
                     <MenubarSub>
                         <MenubarSubTrigger>Partager</MenubarSubTrigger>
                         <MenubarSubContent>
@@ -282,22 +285,22 @@ const MainMenubar = () => {
                             <MenubarItem>X</MenubarItem>
                         </MenubarSubContent>
                     </MenubarSub>
-                    <MenubarSeparator/>
+                    <MenubarSeparator />
                 </MenubarContent>
             </MenubarMenu>
             <div className={"w-full"}></div>
             <MenubarMenu>
                 <MenubarTrigger className={"ml-auto"}>
-                    <SearchIcon className="h-4 w-4 mr-1.5"/>
+                    <SearchIcon className="h-4 w-4 mr-1.5" />
                     <Input type="search" placeholder="Search a NFT"
-                           className="ml-auto border-0 h-8 font-semibold"
-                           /*todo onSubmit={}*//>
+                        className="ml-auto border-0 h-8 font-semibold"
+                           /*todo onSubmit={}*/ />
                 </MenubarTrigger>
             </MenubarMenu>
             <MenubarMenu>
                 <MenubarTrigger>
                     <Avatar>
-                        <AvatarImage src="https://github.com/shadcn.png"/>
+                        <AvatarImage src="https://github.com/shadcn.png" />
                         <AvatarFallback>CN</AvatarFallback>
                     </Avatar>
                 </MenubarTrigger>
